b3944ccbb4739d51ae95b4320129403e9fac56cf
FreedTapestry21 <freed@freeddev.org>
2023-06-04 01:13:10 +0200

    Removed resolution warning

--- js/index.js
+++ js/index.js
@@ -22,24 +22,8 @@ if (window.innerHeight < 650) {
     document.getElementById("footer").remove();
 } 
 
-if (window.localStorage.getItem("horizontal-res-warn") == null) {window.localStorage.setItem("horizontal-res-warn", "0");}
-if (window.localStorage.getItem("vertical-res-warn") == null) {window.localStorage.setItem("vertical-res-warn", "0");}
-
-if (window.innerWidth < 450) {
-    if (window.localStorage.getItem("horizontal-res-warn") == 0) {
-        alert("Warning: Your horizontal resolution is below the recomended horizontal resolution. You might get a poor experience.");
-        window.localStorage.setItem("horizontal-res-warn", "1");
-    }
-}
-if (window.innerHeight < 550) {
-    if (window.localStorage.getItem("vertical-res-warn") == 0) {
-        alert("Warning: Your vertical resolution is below the recomended vertical resolution. You might get a poor experience.");
-        window.localStorage.setItem("vertical-res-warn", "1");
-    }
-}
-
 /*
-    Add animations
+    Disable animations for the first second after site is loaded
 */
 
 setTimeout(function () {
