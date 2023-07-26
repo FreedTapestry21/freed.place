6ac41e6a304e245f8ba159cb6b7a7190cc9aecea
FreedTapestry21 <freedtapestry21@freed.place>
2023-07-26 02:03:38 +0200

    Better compatibility with mobile

--- js/index.js
+++ js/index.js
@@ -15,17 +15,9 @@ console.log("Hello world!");
     Responsive Elements
 */
 
-if (window.innerWidth < 500) {
-    document.getElementById("menu-div").innerHTML = "<br><br>";
-} 
-if (window.innerHeight < 650) {
-    document.getElementById("footer").remove();
-} 
-
-/*
-    Disable animations for the first second after site is loaded
-*/
-
-setTimeout(function () {
-    document.body.classList.remove("preload");
-}, 1000);
+if (window.innerWidth < 464) {
+    console.log("Making modificatons for smaller screens...")
+    document.getElementById("menu-name").remove();
+    document.getElementById("menu-buttons").classList.remove("right");
+    document.getElementById("menu-buttons").classList.add("center");
+} else {console.log("Seems like no modifications are needed!")}
