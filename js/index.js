793f83c2b155e905e4a4bb299129cb31f6ac275c
FreedTapestry21 <freedtapestry21@pixelated.sh>
2024-06-18 20:48:22 +0200

    Updated version

--- js/index.js
+++ js/index.js
@@ -1,23 +1,16 @@
 /*
     Freeddev - Index script
-    Version 2.0
+    Version 2.1-dev
     Copyright (c) 2022-2024 FreedTapstry21
 */
 
-/* 
-    This script runs at every website.
-*/
-
-/* Hello world */
-console.log("Hello world!");
-
 /*
-    Responsive Elements
+    Responsive Element(s)
 */
 
 if (window.innerWidth < 464) {
-    console.log("Making modificatons for smaller screens...")
     document.getElementById("menu-name").remove();
     document.getElementById("menu-buttons").classList.remove("right");
     document.getElementById("menu-buttons").classList.add("center");
-} else {console.log("Seems like no modifications are needed!")}
+    console.log("Enabled menu modification for smaller screens.")
+} else {console.log("No modifications have been made.")}
